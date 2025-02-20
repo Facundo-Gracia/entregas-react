bb1f0b1481e3a0d77d3daae3f85e260de3f9c103
Facundo Gracia <facundogracia01@gmail.com>
2025-02-20 19:01:13 -0300

    Cards agregadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
 import './App.css';
 
 function App() {
-  let nombreGay = "Fak"
   return (
-    <h1>Bienvenido {nombreGay}</h1>
+    <>
+    <header>
+      <nav className="barra">
+        <p>Logo de la empresa</p>
+        <ul className='nav-bar'>
+          <li>Inicio</li>
+          <li>Tortas</li>
+          <li>Galletitas</li>
+        </ul>
+          <img className='carrito' src="https://cdn-icons-png.flaticon.com/512/3144/3144456.png" alt=""/>
+      </nav>
+    </header>
+    <main>
+      <div className='container-cards'>
+        <div className='cards'>
+          <h2>Torta oreo</h2>
+          <p>$10.000</p>
+          <button>Comprar</button>
+        </div>
+        <div className='cards'>
+          <h2>Torta ricota</h2>
+          <p>$15.000</p>
+          <button>Comprar</button>
+        </div>
+        <div className='cards'>
+          <h2>Tiramisu</h2>
+          <p>$30.000</p>
+          <button>Comprar</button>
+        </div>
+      </div>
+    </main>
+    {/* <footer className='footer'>
+  
+    </footer> */}
+    </>
   );
 };
 
